e72de233e88d24221e005476a8e5d3917eafd00a
notator <j.ingram@netcologne.de>
2025-08-06 13:41:50 +0200

    Pianola Music (version 2025) - 3 staves now runs correctly.

--- es6/Controls.js
+++ es6/Controls.js
@@ -985,7 +985,7 @@ export class Controls
                         scoreInfo.aboutURL = "https://james-ingram-act-two.de/compositions/pianolaMusic/aboutPianolaMusic.html";
                         break;
                     case PIANOLA_MUSIC_3STAVES_SCORE_INDEX:
-                        scoreInfo.filename = "Pianola Music - 3 staves (version 2025)/Pianola Music - 3 staves (version 2025).svg";
+                        scoreInfo.filename = "Pianola Music (version 2025) - 3 staves/Pianola Music (version 2025) - 3 staves.svg";
                         scoreInfo.aboutText = "about Pianola Music";
                         scoreInfo.aboutURL = "https://james-ingram-act-two.de/compositions/pianolaMusic/aboutPianolaMusic.html";
                         break;
