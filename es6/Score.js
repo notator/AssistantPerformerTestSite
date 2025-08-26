90031142c807f31a70a1e5ce3a3105a78d95610d
notator <j.ingram@netcologne.de>
2025-08-26 14:58:28 +0200

    Revising Score.setTrackPerformanceObjects()...
    First step: new function setRegionMidiObjectIndexRangePerTrack(...)
    Sets a new .regionRanges array attribute for each track, with one value per region in sequence:
    Each: regionRange has two attributes:
    .firstMidiObjectIndex and
    .lastMidiObjectIndex
    (This is now working for the three test scores.)
    TODO: construct the flat list of trackPerformanceObjects for each track.

--- es6/Score.js
+++ es6/Score.js
@@ -1818,14 +1818,13 @@ let //**************************************************************************
                         let barlines = systems[systemIndex].barlinesPerInterpretation[0];
                         for(let barline of barlines)
                         {
-                            if((region.startMsPosInScore === 0)
-                                || (barline.typeString === "startRegionBarline" && region.startMsPosInScore === barline.msPositionInScore))
+                            if(region.startMsPosInScore === barline.msPositionInScore)
                             {
                                 region.startBarline = barline;
                                 region.systemIndex = systemIndex;
                                 found = true;
                                 break;
-                            }
+                            }                            
                         }
                         if(found)
                         {
@@ -1930,102 +1929,122 @@ let //**************************************************************************
         return tracks.length;
     },
 
-    //// should return all tracks with region sequences included
-    //// 22.08.2025 Returns a flat list of of MidiChords and MidiRests derived from the region definitions.
-	//// The Sequence code should no longer have anything to do with regions!
-    //getMidiObjectsPerTrack = function ()
-    //{
-    //    let allTracks = tracks,
-    //        currentTracks = [],
-    //        region = regionSequence[currentRegionIndex],
-    //        currentInterpretationIndex;
-    //
-    //    if(region.isSimpleInterpretation())
-    //    {
-    //        currentInterpretationIndex = regionSequence[currentRegionIndex].interpIndex;
-    //    }
-    //
-    //    for(let i = 0; i < allTracks.length; ++i)																				  
-    //    {
-    //        currentTracks.push(allTracks[i].interpretations[currentInterpretationIndex]);
-    //    }
-    //
-    //    return currentTracks;
-    //},
 
     setTrackPerformanceObjects = function()
     {
-        function setRegionSequenceMsPositionsInPerformance()
-        {            
-            function getMsDuration(interpretations, interpIndex)
-            {
-                let msDur = 0,
-                    midiObjects = interpretations[interpIndex].midiObjects;
-
-                for(let midiObject of midiObjects)
-                {
-                    msDur += midiObject.msDurationInScore;
-                }
-
-                return msDur;
-            }
-
-            let msPosInPerf = 0,
-                regionDurInPerf,
-                interpIndex = -1,
-                interpretations = tracks[0].interpretations;
-
-            for(let region of regionSequence)
-            {
-                interpIndex = region.interpIndex;
-                regionDurInPerf = getMsDuration(interpretations, interpIndex);
-                region.startMsPosInPerf = msPosInPerf;
-                msPosInPerf += regionDurInPerf;
-                region.endMsPosInPerf = msPosInPerf;
-            }
-        }
-
-        function findMidiObjectIndexRange(region, track)
+        function setRegionMidiObjectIndexRangePerTrack(regionSequence, track)
         {
-            let midiObjects = track.interpretations[0].midiObjects,                
-                firstIndex = midiObjects.findIndex(x => (x.msPositionInScore >= region.startMsPosInScore && x.msPositionInScore < region.endMsPosInScore)),
-                lastIndex  = midiObjects.findLastIndex(x => (x.msPositionInScore >= region.startMsPosInScore && x.msPositionInScore < region.endMsPosInScore));
-             
-            return {firstIndex, lastIndex};
+            let midiObjects = track.interpretations[0].midiObjects;
 
+            track.regionRanges = [];
+
+            for(let i = 0; i < regionSequence.length; i++)
+            {
+                let region = regionSequence[i],
+                    nextRegion = (i < regionSequence.length - 1) ? regionSequence[i + 1] : null,
+                    regionStartMsPos = region.startMsPosInScore,
+                    regionEndMsPos = region.endMsPosInScore,
+                    regionRange = {};
+                    
+                regionRange.firstMidiObjectIndex = midiObjects.findIndex(x => (x.msPositionInScore >= regionStartMsPos && x.msPositionInScore < regionEndMsPos));
+                regionRange.lastMidiObjectIndex = midiObjects.findLastIndex(x => (x.msPositionInScore >= regionStartMsPos && x.msPositionInScore < regionEndMsPos));
+
+                track.regionRanges.push(regionRange);
+            }
         }
 
-        setRegionSequenceMsPositionsInPerformance();
-        
-        let performanceDuration = regionSequence[regionSequence.length-1].endMsPosInPerf;
+        //function setRegionSequenceMsPositionsInPerformance()
+        //{            
+        //    function getMsDuration(interpretations, interpIndex)
+        //    {
+        //        let msDur = 0,
+        //            midiObjects = interpretations[interpIndex].midiObjects;
+
+        //        for(let midiObject of midiObjects)
+        //        {
+        //            msDur += midiObject.msDurationInScore;
+        //        }
+
+        //        return msDur;
+        //    }
+
+        //    let msPosInPerf = 0,
+        //        regionDurInPerf,
+        //        interpIndex = -1,
+        //        interpretations = tracks[0].interpretations,
+        //        regionIndexForLog = 0;
+
+
+        //    for(let region of regionSequence)
+        //    {
+        //        console.log("regionIndex= " + regionIndexForLog.toString());
+        //        regionIndexForLog++;
+
+        //        interpIndex = region.interpIndex;
+        //        regionDurInPerf = getMsDuration(interpretations, interpIndex);
+        //        region.startMsPosInPerf = msPosInPerf;
+        //        msPosInPerf += regionDurInPerf;
+        //        region.endMsPosInPerf = msPosInPerf;
+
+        //        console.log("   region.startMsPosInPerf= " + region.startMsPosInPerf.toString());
+        //        console.log("   region.endMsPosInPerf= " + region.endMsPosInPerf.toString());
+        //    }
+        //}
 
         for(let track of tracks)
         {
-            let performedMidiObjects = [],
-                msPosInPerf = 0;
-
-            for(let region of regionSequence)
-            {
-                let indexRange = findMidiObjectIndexRange(region, track),
-                    firstIndex = indexRange.firstIndex,
-                    lastIndex = indexRange.lastIndex, 
-                    midiObjects = track.interpretations[region.interpIndex].midiObjects;
-                
-                for(let moIndex = firstIndex; moIndex <= lastIndex; moIndex++)
-                {
-                    let midiObject = midiObjects[moIndex];
-                    midiObject.msPosInPerf = msPosInPerf;
-                    midiObject.msDurInPerf = midiObject.msDurationInScore;
-                    msPosInPerf += midiObject.msDurInPerf;
-
-                    performedMidiObjects.push(midiObject);
-                }
-            }
-            track.performedMidiObjects = performedMidiObjects;
-
-            let lastMidiObject = performedMidiObjects[performedMidiObjects.length - 1];
-            console.assert((lastMidiObject.msPosInPerf + lastMidiObject.msDurInPerf) === performanceDuration);
+            setRegionMidiObjectIndexRangePerTrack(regionSequence, track);
         }
+        
+        //let performanceDuration = regionSequence[regionSequence.length - 1].endMsPosInPerf;
+
+        //console.log("performanceDuration= " + performanceDuration.toString());
+
+        //let trackIndexForLog = 0;
+
+        //for(let track of tracks)
+        //{
+        //    let performedMidiObjects = [],
+        //        msPosInPerf = 0,
+        //        regionIndexForLog = 0;
+
+        //    console.log("trackIndex= " + trackIndexForLog.toString());
+        //    trackIndexForLog++;
+
+        //    for(let region of regionSequence)
+        //    {
+        //        let indexRange = findMidiObjectIndexRange(region, track),
+        //            firstIndex = indexRange.firstIndex,
+        //            lastIndex = indexRange.lastIndex, 
+        //            midiObjects = track.interpretations[region.interpIndex].midiObjects,
+        //            regionDurationForLog = 0;
+
+        //        console.log("   regionIndex= " + regionIndexForLog.toString());
+        //        regionIndexForLog++;
+        //        console.log("      firstIndex= " + firstIndex.toString());
+        //        console.log("      lastIndex= " + lastIndex.toString());
+                
+        //        for(let moIndex = firstIndex; moIndex <= lastIndex; moIndex++)
+        //        {
+        //            let midiObject = midiObjects[moIndex];
+        //            midiObject.msPosInPerf = msPosInPerf;
+        //            midiObject.msDurInPerf = midiObject.msDurationInScore;
+        //            msPosInPerf += midiObject.msDurInPerf;
+
+        //            regionDurationForLog += midiObject.msDurInPerf;
+
+        //            performedMidiObjects.push(midiObject);
+        //        }
+        //        console.log("      regionDurationForLog= " + regionDurationForLog.toString());
+        //    }
+        //    track.performedMidiObjects = performedMidiObjects;
+
+        //    let lastMidiObject = performedMidiObjects[performedMidiObjects.length - 1],
+        //        endOfLastObject = lastMidiObject.msPosInPerf + lastMidiObject.msDurInPerf;
+
+        //    console.log("endOfLastObject= " + endOfLastObject.toString()) + " (should be equal to performanceDuration)";
+        //    //console.assert(endOfLastObject === performanceDuration);
+        //}
     },
 
     getMarkersLayer = function ()
