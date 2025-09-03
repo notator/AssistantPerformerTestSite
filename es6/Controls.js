f34c847ff64be41a97ac37c538851d5c471fa003
notator <j.ingram@netcologne.de>
2025-09-03 14:30:31 +0200

    Edited comment.

--- es6/Controls.js
+++ es6/Controls.js
@@ -1438,9 +1438,8 @@ export class Controls
 
         player = new Sequence(); // TODO move the Sequence.init() code into the Sequence constructor.
 
-        // 03.09.2025 TODO: the arguments to the following function need to change.
-        // I think it should just be given outputDevice and the callbacks.
-        // The score.moments should be given to the player.play() function.
+        // 03.09.2025 The arguments to the following function have changed to no longer include score.
+        // The score.moments should now be given to the player.play() function.
         player.init(deviceOptions.outputDevice, reportEndOfRegion, reportEndOfPerformance, reportMsPos, score.reportTickOverload);
 
         setSvgControlsState('stopped');
