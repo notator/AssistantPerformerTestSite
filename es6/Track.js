7736719e0089ad125545acb972ae777caf9e30a0
notator <j.ingram@netcologne.de>
2025-10-02 18:13:07 +0200

    Missing semicolon

--- es6/Track.js
+++ es6/Track.js
@@ -41,7 +41,7 @@ export class Track
 
 		function getCurrentInterpretation(that, regionSequence, currentRegionIndex, midiObjectIndexRangesPerRegion)
 		{
-			console.assert(regionSequence.hasConsecutiveRegions === false)
+			console.assert(regionSequence.hasConsecutiveRegions === false);
 
 			let interpretation = new Interpretation(),
 				region = regionSequence[currentRegionIndex],
